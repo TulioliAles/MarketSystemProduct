f0aed3a1e61a498edf5bff09998a6f8957798e94
Alessandro Tulioli <alessandro.tulioli@csu.com.br>
2025-04-14 20:49:16 -0300

    Inicio do projeto

---
+++ MarketSystemProduct/Migrations/20250414233752_AdicionandoBancoDados.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketSystemProduct.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoBancoDados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
